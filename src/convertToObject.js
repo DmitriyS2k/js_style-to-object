5e20ccb52a9ba8e925c33d85c14bab0a344aca6e
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-04-08 14:15:43 +0300

    add js task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+  const arrOfStyles = sourceString
+    .split(';')
+    .filter((str) => str.includes(':'));
+
+  arrOfStyles.forEach((styleStr) => {
+    const [key, value] = styleStr.split(':');
+    const styleKey = key.trim();
+    const styleValue = value.trim();
+
+    stylesObject[styleKey] = styleValue;
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
